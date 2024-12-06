c1faf9fee172b58473360031e2e39de7571c21ff
edSantos <profedacc@gmail.com>
2024-12-06 10:51:29 +0000

    Update UpdateDatabase.js

--- Projeto/JS/UpdateDatabase.js
+++ Projeto/JS/UpdateDatabase.js
@@ -12,24 +12,21 @@ const pool = mysql.createPool({
 
 // Função para atualizar/inserir em Companies
 async function updateCompanies(data) {
-    // Update
-    const updateCompany = `UPDATE companies SET Name = ?, Description = ?, Legal = ?, DUNS = ?, VAT = ?, Score = ?, Sentiment = ?, Status = 1, Source = ?, Updated_at = CURRENT_TIMESTAMP WHERE VAT = ?`;
-    const [result] = await pool.execute(updateCompany, [
-        data.NameCompany,
-        data.DescriptionCompany,
-        data.LegalCompany,
-        data.DUNS,
-        data.VAT,
-        data.ScoreCompany,
-        data.SentimentCompany,
-        data.Source,
-        data.VAT
-    ]);
-    if (result.affectedRows === 0) {
-        // Insert
-        const insertCompany = `INSERT INTO companies (Name, Description, Legal, DUNS, VAT, Score, Sentiment, Status, Source, Created_at, Updated_at) 
-                               VALUES (?, ?, ?, ?, ?, ?, ?, 1, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`;
-        const [insertResult] = await pool.execute(insertCompany, [
+    const checkSourceQuery = `SELECT Source FROM companies WHERE VAT = ?`;
+    const [existingRows] = await pool.execute(checkSourceQuery, [data.VAT]);
+
+    if (existingRows.length > 0) {
+        const existingSource = existingRows[0].Source;
+
+        // Ignora a atualização apenas se a fonte existente for 'Racius' e a nova for 'Einforma'
+        if (existingSource === 'Racius' && data.Source === 'Einforma') {
+            console.log(`Atualização ignorada: NIF: ${data.VAT}`);
+            return;
+        }
+
+        // Update
+        const updateCompany = `UPDATE companies SET Name = ?, Description = ?, Legal = ?, DUNS = ?, VAT = ?, Score = ?, Sentiment = ?, Status = 1, Source = ?, Updated_at = CURRENT_TIMESTAMP WHERE VAT = ?`;
+        await pool.execute(updateCompany, [
             data.NameCompany,
             data.DescriptionCompany,
             data.LegalCompany,
@@ -37,39 +34,47 @@ async function updateCompanies(data) {
             data.VAT,
             data.ScoreCompany,
             data.SentimentCompany,
-            data.Source
+            data.Source,
+            data.VAT
         ]);
-        // Response
-        console.log(`Empresa inserida, NIF empresa: ${data.VAT}.`);
-        return
+        console.log(`Atualizado. NIF: ${data.VAT}`);
+        return;
     }
-    // Response
-    console.log(`Empresa atualizada, NIF empresa: ${data.VAT}.`);
+    // Insert
+    const insertCompany = `INSERT INTO companies (Name, Description, Legal, DUNS, VAT, Score, Sentiment, Status, Source, Created_at, Updated_at) 
+                           VALUES (?, ?, ?, ?, ?, ?, ?, 1, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`;
+    await pool.execute(insertCompany, [
+        data.NameCompany,
+        data.DescriptionCompany,
+        data.LegalCompany,
+        data.DUNS,
+        data.VAT,
+        data.ScoreCompany,
+        data.SentimentCompany,
+        data.Source
+    ]);
+    console.log(`Inserido. NIF: ${data.VAT}`);
 }
+
 
 
 
 // Função para atualizar/inserir em Brands
 async function updateBrands(data, idCompany) {
-    // Update
-    const updateBrand = `UPDATE brands SET Name = ?, Description = ?, Logo = ?, VAT = ?, Score = ?, NumReviews = ?, Sentiment = ?, Status = 1, Source = ?, Updated_at = CURRENT_TIMESTAMP, IDCompany = ?, IsPrimary = 1 WHERE VAT = ?`;
-    const [result] = await pool.execute(updateBrand, [
-        data.NameBrand,
-        data.DescriptionBrand,
-        data.Logo,
-        data.VAT,
-        data.ScoreBrand,
-        data.NumReviews,
-        data.SentimentBrand,
-        data.Source,
-        idCompany,
-        data.VAT
-    ]);
-    if (result.affectedRows === 0) {
-        // Insert
-        const insertBrand = `INSERT INTO brands (Name, Description, Logo, VAT, Score, NumReviews, Sentiment, Status, Source, Created_at, Updated_at, IDCompany, IsPrimary) 
-                             VALUES (?, ?, ?, ?, ?, ?, ?, 1, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ?, 1)`;
-        const [insertResult] = await pool.execute(insertBrand, [
+    const checkSourceQuery = `SELECT Source FROM brands WHERE VAT = ?`;
+    const [existingRows] = await pool.execute(checkSourceQuery, [data.VAT]);
+
+    if (existingRows.length > 0) {
+        const existingSource = existingRows[0].Source;
+
+        // Ignora a atualização apenas se a fonte existente for 'Racius' e a nova for 'Einforma'
+        if (existingSource === 'Racius' && data.Source === 'Einforma') {
+            console.log(`Atualização ignorada: NIF: ${data.VAT}`);
+            return;
+        }
+        // Update
+        const updateBrand = `UPDATE brands SET Name = ?, Description = ?, Logo = ?, VAT = ?, Score = ?, NumReviews = ?, Sentiment = ?, Status = 1, Source = ?, Updated_at = CURRENT_TIMESTAMP, IDCompany = ?, IsPrimary = 1 WHERE VAT = ?`;
+        await pool.execute(updateBrand, [
             data.NameBrand,
             data.DescriptionBrand,
             data.Logo,
@@ -78,39 +83,48 @@ async function updateBrands(data, idCompany) {
             data.NumReviews,
             data.SentimentBrand,
             data.Source,
-            idCompany
+            idCompany,
+            data.VAT
         ]);
-        // Response
-        console.log(`Marca inserida, NIF empresa: ${data.VAT}.`);
-        return
+        console.log(`Atualizado. NIF: ${data.VAT}`);
+        return;
     }
-    // Response
-    console.log(`Marca atualizada, NIF empresa: ${data.VAT}.`);
+    // Insert
+    const insertBrand = `INSERT INTO brands (Name, Description, Logo, VAT, Score, NumReviews, Sentiment, Status, Source, Created_at, Updated_at, IDCompany, IsPrimary) 
+                         VALUES (?, ?, ?, ?, ?, ?, ?, 1, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ?, 1)`;
+    await pool.execute(insertBrand, [
+        data.NameBrand,
+        data.DescriptionBrand,
+        data.Logo,
+        data.VAT,
+        data.ScoreBrand,
+        data.NumReviews,
+        data.SentimentBrand,
+        data.Source,
+        idCompany
+    ]);
+    console.log(`Inserido. NIF: ${data.VAT}`);
 }
+
 
 
 
 // Função para atualizar/inserir em Addresses
 async function updateAddresses(data, idBrand) {
-    // Update
-    const updateAddress = `UPDATE addresses SET Address = ?, Location = ?, Zipcode = ?, County = ?, District = ?, Country = ?, Parish = ?, Status = 1, Updated_at = CURRENT_TIMESTAMP, IsPrimary = 1, Source = ? WHERE IDBrand = ?`;
-    const [result] = await pool.execute(updateAddress, [
-        data.Address,
-        data.Location,
-        data.Zipcode,
-        data.County,
-        data.District,
-        data.Country,
-        data.Parish,
-        data.Source,
-        idBrand
-    ]);
-    if (result.affectedRows === 0) {
-        // Insert
-        const insertAddress = `INSERT INTO addresses (IDBrand, Address, Location, Zipcode, County, District, Country, Parish, Status, Created_at, Updated_at, IsPrimary, Source) 
-                               VALUES (?, ?, ?, ?, ?, ?, ?, ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, ?)`;
-        const [insertResult] = await pool.execute(insertAddress, [
-            idBrand,
+    const checkSourceQuery = `SELECT Source FROM addresses WHERE IDBrand = ?`;
+    const [existingRows] = await pool.execute(checkSourceQuery, [idBrand]);
+
+    if (existingRows.length > 0) {
+        const existingSource = existingRows[0].Source;
+
+        if (existingSource === 'Racius' && data.Source === 'Einforma') {
+            console.log(`Atualização ignorada: IDMarca: ${idBrand}`);
+            return;
+        }
+
+        // Update
+        const updateAddress = `UPDATE addresses SET Address = ?, Location = ?, Zipcode = ?, County = ?, District = ?, Country = ?, Parish = ?, Status = 1, Updated_at = CURRENT_TIMESTAMP, IsPrimary = 1, Source = ? WHERE IDBrand = ?`;
+        await pool.execute(updateAddress, [
             data.Address,
             data.Location,
             data.Zipcode,
@@ -118,70 +132,102 @@ async function updateAddresses(data, idBrand) {
             data.District,
             data.Country,
             data.Parish,
-            data.Source
+            data.Source,
+            idBrand
         ]);
-        // Response
-        console.log(`Morada inserida, Marca ID: ${data.idBrand}.`);
-        return
+        console.log(`Atualizado. IDMarca: ${idBrand}`);
+        return;
     }
-    // Response
-    console.log(`Morada atualizada, Marca ID: ${data.idBrand}.`);
+    // Insert
+    const insertAddress = `INSERT INTO addresses (IDBrand, Address, Location, Zipcode, County, District, Country, Parish, Status, Created_at, Updated_at, IsPrimary, Source) 
+                           VALUES (?, ?, ?, ?, ?, ?, ?, ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, ?)`;
+    await pool.execute(insertAddress, [
+        idBrand,
+        data.Address,
+        data.Location,
+        data.Zipcode,
+        data.County,
+        data.District,
+        data.Country,
+        data.Parish,
+        data.Source
+    ]);
+    console.log(`Inserido. IDMarca: ${idBrand}`);
 }
 
 
 
 // Função para atualizar/inserir em Contacts
 async function updateContacts(data, idBrand) {
-    const updateContact = `UPDATE contacts SET Type = ?, Status = 1, Updated_at = CURRENT_TIMESTAMP, IsPrimary = 1, Source = ? WHERE IDBrand = ? AND Contact = ?`;
-    const [result] = await pool.execute(updateContact, [
-        data.TypeContact,
-        data.Source,
-        idBrand,
-        data.Contact
-    ]);
-    if (result.affectedRows === 0) {
-        const insertContact = `INSERT INTO contacts (Contact, Type, Status, Created_at, Updated_at, IsPrimary, IDBrand, Source) 
-                               VALUES (?, ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, ?, ?)`;
-        const [insertResult] = await pool.execute(insertContact, [
-            data.Contact,
+    const checkSourceQuery = `SELECT Source FROM contacts WHERE IDBrand = ? AND Contact = ?`;
+    const [existingRows] = await pool.execute(checkSourceQuery, [idBrand, data.Contact]);
+
+    if (existingRows.length > 0) {
+        const existingSource = existingRows[0].Source;
+
+        if (existingSource === 'Racius' && data.Source === 'Einforma') {
+            console.log(`Atualização ignorada: Fonte prioritária 'Racius'. IDMarca: ${idBrand}`);
+            return;
+        }
+
+        // Update
+        const updateContact = `UPDATE contacts SET Type = ?, Status = 1, Updated_at = CURRENT_TIMESTAMP, IsPrimary = 1, Source = ? WHERE IDBrand = ? AND Contact = ?`;
+        await pool.execute(updateContact, [
             data.TypeContact,
+            data.Source,
             idBrand,
-            data.Source
+            data.Contact
         ]);
-        // Response
-        console.log(`Contacto inserido, Marca ID: ${data.idBrand}.`);
-        return
+        console.log(`Atualizado. IDMarca: ${idBrand}`);
+        return;
     }
-    // Response
-    console.log(`Contacto atualizado, Marca ID: ${data.idBrand}.`);
+    // Insert
+    const insertContact = `INSERT INTO contacts (Contact, Type, Status, Created_at, Updated_at, IsPrimary, IDBrand, Source) 
+                           VALUES (?, ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, ?, ?)`;
+    await pool.execute(insertContact, [
+        data.Contact,
+        data.TypeContact,
+        idBrand,
+        data.Source
+    ]);
+    console.log(`Inserido. IDMarca: ${idBrand}`);
 }
 
 
 
 // Função para atualizar/inserir em Links
 async function updateLinks(data, idBrand) {
-    const updateLink = `UPDATE links SET Type = ?, Status = 1, Updated_at = CURRENT_TIMESTAMP, Source = ? WHERE IDBrand = ? AND Url = ?`;
-    const [result] = await pool.execute(updateLink, [
-        data.TypeLink,
-        data.Source,
-        idBrand,
-        data.Url
-    ]);
-    if (result.affectedRows === 0) {
-        const insertLink = `INSERT INTO links (Url, Type, Status, Created_at, Updated_at, IDBrand, Source) 
-                            VALUES (?, ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ?, ?)`;
-        const [insertResult] = await pool.execute(insertLink, [
-            data.Url,
+    const checkSourceQuery = `SELECT Source FROM links WHERE IDBrand = ? AND Url = ?`;
+    const [existingRows] = await pool.execute(checkSourceQuery, [idBrand, data.Url]);
+
+    if (existingRows.length > 0) {
+        const existingSource = existingRows[0].Source;
+
+        if (existingSource === 'Racius' && data.Source === 'Einforma') {
+            console.log(`Atualização ignorada: Fonte prioritária 'Racius'. IDMarca: ${idBrand}`);
+            return;
+        }
+        // Update
+        const updateLink = `UPDATE links SET Type = ?, Status = 1, Updated_at = CURRENT_TIMESTAMP, Source = ? WHERE IDBrand = ? AND Url = ?`;
+        await pool.execute(updateLink, [
             data.TypeLink,
+            data.Source,
             idBrand,
-            data.Source
+            data.Url
         ]);
-        // Response
-        console.log(`"Link" inserido, Marca ID: ${data.idBrand}.`);
-        return
+        console.log(`Atualizado. IDMarca: ${idBrand}`);
+        return;
     }
-    // Response
-    console.log(`"Link" atualizado, Marca ID: ${data.idBrand}.`);
+    // Insert
+    const insertLink = `INSERT INTO links (Url, Type, Status, Created_at, Updated_at, IDBrand, Source) 
+                        VALUES (?, ?, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, ?, ?)`;
+    await pool.execute(insertLink, [
+        data.Url,
+        data.TypeLink,
+        idBrand,
+        data.Source
+    ]);
+    console.log(`Inserido. IDMarca: ${idBrand}`);
 }
 
 
